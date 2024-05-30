c57aadea28150eae5a42f0179d7e512ebe9798ad
Elliott <ellbutt@gmail.com>
2024-05-30 10:32:54 -0230

    added init features

--- init.js
+++ init.js
@@ -1,5 +1,64 @@
 const fs = require("fs");
+const fsPromises = require("fs").promises;
+const path = require("path");
+
 const myArgs = process.argv.slice(2);
+
+const folders = ["models", "views", "routes", "logs", "json"];
+
+const configjson = {
+  name: "AppConfigCLI",
+  version: "1.0.0",
+  description: "The Command Line Interface (CLI) for the MyApp.",
+  main: "myapp.js",
+  superuser: "adm1n",
+  database: "exampledb",
+};
+
+function createFiles() {
+  if (DEBUG) console.log("init.createFiles()");
+  try {
+    let configdata = JSON.stringify(configjson, null, 2);
+    if (!fs.existsSync(path.join(__dirname, "./json/config.json"))) {
+      fs.writeFile("./json/config.json", configdata, (err) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Data written to config file.");
+        }
+      });
+    } else {
+      console.log("config file already exists.");
+    }
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+function createFolders() {
+  if (DEBUG) console.log("init.createFolders()");
+  let mkcount = 0;
+  folders.forEach((element) => {
+    if (DEBUG) console.log(element);
+    try {
+      if (!fs.existsSync(path.join(__dirname, element))) {
+        fsPromises.mkdir(path.join(__dirname, element));
+        mkcount++;
+      }
+    } catch (err) {
+      console.log(err);
+    }
+  });
+
+  if (mkcount === 0) {
+    if (DEBUG) console.log("All folders already exist.");
+  } else if (mkcount <= folders.length) {
+    if (DEBUG)
+      console.log(mkcount + " of " + folders.length + " folders were created.");
+  } else {
+    if (DEBUG) console.log("All folders successfully created.");
+  }
+}
 
 function initializeApplication() {
   if (DEBUG) console.log("initializeApplication()");
@@ -10,9 +69,11 @@ function initializeApplication() {
       break;
     case "--cat":
       if (DEBUG) console.log("--cat createFiles()");
+      createFiles();
       break;
     case "--mk":
       if (DEBUG) console.log("--mk createFolders()");
+      createFolders();
       break;
     case "--help":
     case "--h":
